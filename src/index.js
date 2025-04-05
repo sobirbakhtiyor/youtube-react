8f3dad8be4c3a5ff5255a9deec7a8a42bba91a88
sobirbakhtiyor <sobirbakhtiyor@gmail.com>
2025-04-05 14:34:06 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App'
 
 ReactDOM.render(
-  // {/*<App />*/},
-   <div>Site under construction...</div>,
+  <App />,
   document.querySelector('#root')
 );
